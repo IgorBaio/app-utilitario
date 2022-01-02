acb625d62c52317debb534da14b7d92a41dd7694
IgorBaio <igorbaiosoares@gmail.com>
2022-01-01 21:22:36 -0300

    Criação de um componente Input

---
+++ commonStyles.js
@@ -0,0 +1,15 @@
+export default {
+    fontFamily:'Lato',
+    colors: {
+        today:'#422E47',
+        secundary: '#FFF',
+        tertiary: '#FEB627',
+        mainText:'#222',
+        subText:'#555',
+        background:'rgb(223,156,72)',
+        seondaryBackground:'rgb(50,71,241)',
+        textButtons: 'rgb(174,50,46)',
+        backgroundOpactiy70: 'rgba(254,217,217,0.7)',
+        backgroundOpactiy40: 'rgba(224,120,121,0.4)'
+    }
+}
---
+++ src/components/Input.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components/native";
+import IconMaterial from "react-native-vector-icons/MaterialIcons";
+import commonStyles from "../../commonStyles";
+import colors from "../../utils/colors";
+const InputArea = styled.View`
+  width: 90%;
+  height: 60px;
+  background-color: transparent;
+  flex-direction: row;
+  padding-left: 15px;
+  align-items: center;
+  justify-content: center;
+  border: 2px;
+  border-color: #fff;
+  border-radius: 18;
+  margin-left: 4%;
+  margin-right: 2%;
+`;
+
+const Input = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: ${(props) => props.colorInput || "#FFF"};
+  margin-left: 10px;
+`;
+
+export default ({
+  IconName,
+  placeholder,
+  value,
+  onChangeText,
+  password,
+  borderColor,
+}) => {
+  return (
+    <InputArea
+      colorInput={commonStyles.colors.primary}
+      style={{ borderColor: borderColor }}
+    >
+      {IconName && (
+        <IconMaterial
+          name={IconName}
+          size={25}
+          color={commonStyles.colors.textButtons}
+        />
+      )}
+      <Input
+        colorInput={borderColor || commonStyles.colors.primary}
+        placeholder={placeholder}
+        placeholderTextColor={
+          borderColor ? colors.textPurple : "rgba(255,255,255,0.5)"
+        }
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={password}
+        style={{
+          color: borderColor ? colors.textPurple : "rgba(255,255,255,0.5)",
+        }}
+      />
+    </InputArea>
+  );
+};
